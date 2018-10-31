ec6ebed5d48e432614bb7af077c41ddb4de3f498
Andreas Winter <awi@adorsys.de>
2018-10-31 15:20:32 +0100

    Add linter and pre-hooks

--- ui/src/app/app.component.spec.ts
+++ ui/src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
