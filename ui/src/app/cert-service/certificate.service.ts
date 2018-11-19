0deb81105a0cecf02ca2bd3537d6857fb0fc1e5b
Andreas Winter <awi@adorsys.de>
2018-11-19 09:26:05 +0100

    Use dynamic navigation and inapp routing

--- ui/src/app/cert-service/certificate.service.ts
+++ ui/src/app/cert-service/certificate.service.ts
@@ -8,7 +8,7 @@ import { CertificateRequest } from '../../models/certificateRequest';
   providedIn: 'root'
 })
 export class CertificateService {
-  CREATE_CERT_URL = `/api/cert-generator`;
+  CREATE_CERT_URL = `/xs2a/api/cert-generator`;
   certResponse: CertificateResponse;
 
   constructor(private httpClient: HttpClient) {
