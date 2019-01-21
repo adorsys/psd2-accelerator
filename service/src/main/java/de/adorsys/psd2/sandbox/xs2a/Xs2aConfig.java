93c73e56ebc690acd1dae1ca75a525c89126bd26
Roman Ataman <roman.ataman@adorsys.de>
2019-01-21 16:52:15 +0100

    Use own objectMapper
    There is a conflict with halObjectMapper and the xs2a object Mapper.

--- service/src/main/java/de/adorsys/psd2/sandbox/xs2a/Xs2aConfig.java
+++ service/src/main/java/de/adorsys/psd2/sandbox/xs2a/Xs2aConfig.java
@@ -1,16 +1,19 @@
 package de.adorsys.psd2.sandbox.xs2a;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Sets;
 import de.adorsys.psd2.consent.domain.PsuData;
-import de.adorsys.psd2.consent.domain.TppStopListEntity;
 import de.adorsys.psd2.consent.repository.PsuDataRepository;
-import de.adorsys.psd2.consent.repository.TppStopListRepository;
 import de.adorsys.psd2.sandbox.ContextHolder;
 import de.adorsys.psd2.sandbox.xs2a.web.filter.MockCertificateFilter;
 import de.adorsys.psd2.sandbox.xs2a.web.filter.TabDelimitedCertificateFilter;
+import de.adorsys.psd2.xs2a.config.ObjectMapperConfig;
 import de.adorsys.psd2.xs2a.service.validator.tpp.TppInfoHolder;
 import de.adorsys.psd2.xs2a.service.validator.tpp.TppRoleValidationService;
 import de.adorsys.psd2.xs2a.web.filter.QwacCertificateFilter;
-import javax.sql.DataSource;
+import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -19,7 +22,11 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.type.classreading.MetadataReader;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.core.type.filter.TypeFilter;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -41,8 +48,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
         "de.adorsys.psd2.sandbox.portal.testdata"
     },
     excludeFilters = @ComponentScan.Filter(
-        type = FilterType.ASSIGNABLE_TYPE,
-        classes = QwacCertificateFilter.class
+        type = FilterType.CUSTOM, classes = Xs2aConfig.Xs2aComponentFilter.class
     )
 )
 @PropertySource(
@@ -87,4 +93,51 @@ public class Xs2aConfig {
     }
 
   }
+
+  public static class Xs2aComponentFilter implements TypeFilter {
+
+    private static Logger log = LoggerFactory.getLogger(Xs2aComponentFilter.class);
+
+    public static Set<String> blacklist = Sets.newHashSet(
+        ObjectMapperConfig.class.getName(),
+        QwacCertificateFilter.class.getName()
+    );
+
+    @Override
+    public boolean match(MetadataReader metadataReader,
+        MetadataReaderFactory metadataReaderFactory) {
+
+      String className = metadataReader.getClassMetadata().getClassName();
+      if (blacklist.contains(className)) {
+        log.debug("Ignore blacklisted XS2A component: {}", className);
+        return true;
+      }
+
+      return false;
+    }
+  }
+
+  /*
+   * TODO we have to override de.adorsys.psd2.xs2a.config.ObjectMapperConfig because
+   *  a) we can't exclude it (see Xs2aComponentFilter) and
+   *  b) the XS2A ObjectMapperConfig does not mark the OM as @Primary which clashes with
+   *    org.springframework.hateoas.config.HypermediaSupportBeanDefinitionRegistrar
+   *    but somehow only in tests
+   */
+  @Configuration
+  static class SandboxObjectMapperConfig extends ObjectMapperConfig {
+
+    private ObjectMapperConfig original;
+
+    public SandboxObjectMapperConfig(ObjectMapperConfig original) {
+      this.original = original;
+    }
+
+    @Bean
+    @Primary
+    ObjectMapper sandboxObjectMapper() {
+      return super.objectMapper();
+    }
+  }
+
 }
