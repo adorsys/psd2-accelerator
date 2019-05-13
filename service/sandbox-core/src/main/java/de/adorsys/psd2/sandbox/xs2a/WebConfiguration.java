0a586873618073be6536ebec0d3a7a7c764f56d3
Petro Rudenko <pru@adorsys.com.ua>
2019-05-13 23:30:01 +0300

    fix ssl (#107)
    Removed obsolete spring property due to extracting certificate generation service, added web configuration to make resources visible

--- service/sandbox-core/src/main/java/de/adorsys/psd2/sandbox/xs2a/WebConfiguration.java
+++ service/sandbox-core/src/main/java/de/adorsys/psd2/sandbox/xs2a/WebConfiguration.java
@@ -1,16 +1,13 @@
-package de.adorsys.psd2.sandbox.portal.config;
+package de.adorsys.psd2.sandbox.xs2a;
 
+import de.adorsys.psd2.sandbox.certificate.EnableCertificateGeneratorService;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
@@ -18,42 +15,19 @@ import springfox.documentation.swagger.web.UiConfiguration;
 import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
+@EnableCertificateGeneratorService
 @EnableSwagger2
-public class SwaggerConfig {
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter {
 
+  @Value("${sandbox.xs2a.spec}")
   private String xs2aYml;
-
-  public SwaggerConfig(@Value("${sandbox.xs2a.spec}") String xs2aYml) {
-    this.xs2aYml = xs2aYml;
-  }
 
   @Bean
   UiConfiguration uiConfiguration() {
     return UiConfigurationBuilder.builder()
-        .supportedSubmitMethods(new String[]{})
-        .build();
-  }
-
-  @Bean
-  Docket api() {
-    return new Docket(DocumentationType.SWAGGER_2)
-        .apiInfo(new ApiInfoBuilder()
-            .title("Certificate Generator")
-            .description("Certificate Generator for Testing Purposes of PSD2 Sandbox Environment")
-            .contact(new Contact(
-                "swi, adorsys GmbH & Co. KG",
-                "https://adorsys.de",
-                "swi@adorsys.de")
-            )
-            .version("1.0.0")
-            .build())
-        .groupName("Certificate Generator API")
-        .select()
-        .apis(RequestHandlerSelectors
-            .basePackage("de.adorsys.psd2.sandbox.certificate"))
-        .paths(PathSelectors.any())
-        .build();
+               .supportedSubmitMethods(new String[]{})
+               .build();
   }
 
   @Bean
@@ -74,5 +48,14 @@ public class SwaggerConfig {
 
       return resources;
     };
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("swagger-ui.html")
+        .addResourceLocations("classpath:/META-INF/resources/");
+
+    registry.addResourceHandler("/webjars/**")
+        .addResourceLocations("classpath:/META-INF/resources/webjars/");
   }
 }
